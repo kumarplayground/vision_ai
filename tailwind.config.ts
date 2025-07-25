01ed51251f9ee46c8a07d089e11882ad4f0465d8
Rahul Kumar <kumarplayground12345@gmail.com>
2025-07-25 04:28:54 +0000

    i want to increase spinner speed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -101,6 +101,7 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'marquee': 'marquee 40s linear infinite',
+        'spin': 'spin 0.6s linear infinite',
       },
     },
   },
