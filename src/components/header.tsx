389c17dc283e4ca21b4979353f51a360aa430424
Rahul Kumar <kumarplayground12345@gmail.com>
2025-07-31 03:54:09 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,7 @@ const navLinks = [
   { href: "/jobs", label: "Jobs" },
   { href: "/courses", label: "Courses" },
   { href: "/about", label: "About Us" },
+  { href: "/contact", label: "Contact Us" },
 ];
 
 export function Header() {
