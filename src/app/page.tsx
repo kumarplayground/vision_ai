83b33edd0b4cd12aa2ac7aa92081d9129e9116c0
Rahul <kumarplayground12345@gmai.com>
2025-11-03 13:14:10 +0530

    Update sliding logos with partner images and improved container sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,19 +24,37 @@ async function getLatestJobs() {
 
 const CompanyLogo = ({ children }: { children: React.ReactNode }) => (
   <li className="flex-shrink-0">
-    <div className="flex items-center justify-center h-16 w-40 text-muted-foreground">
+    <div className="relative h-28 w-40 mx-4 flex items-center justify-center">
       {children}
     </div>
   </li>
 );
 
 const companies = [
-  { name: 'Innovate Inc.', logo: <svg width="100" height="24" viewBox="0 0 100 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 24C5.37258 24 0 18.6274 0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24ZM34.2857 24V0H42.8571V24H34.2857ZM58.7143 4.28571L51.4286 24H66L73.2857 4.28571L58.7143 4.28571ZM88 12L76 24H100L88 12Z" fill="currentColor"/></svg> },
-  { name: 'TechCorp', logo: <svg width="90" height="24" viewBox="0 0 90 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 24H8.57143V8.57143H17.1429V24H25.7143V0H0V24ZM34.2857 24H42.8571V0H34.2857V24ZM51.4286 24H60V0H51.4286V24ZM68.5714 24H77.1429L81.4286 12L85.7143 24H90L81.4286 0H77.1429L68.5714 24Z" fill="currentColor"/></svg> },
-  { name: 'Solutions Ltd.', logo: <svg width="110" height="24" viewBox="0 0 110 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24H20V0H12ZM42 24H32V0H42V24ZM62 24H52V0H62V24ZM82 24H72V0H82V24ZM102 24H92V0H102C106.418 0 110 3.58172 110 8V16C110 20.4183 106.418 24 102 24Z" fill="currentColor"/></svg> },
-  { name: 'Quantum IT', logo: <svg width="95" height="24" viewBox="0 0 95 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 24V0H8.57143V15.4286H17.1429V0H25.7143V24H0ZM42.8571 24V0H34.2857V24H42.8571ZM60 24H51.4286V0H68.5714V8.57143H60V24ZM95 0H77.1429V24H85.7143V8.57143H95V0Z" fill="currentColor"/></svg> },
-  { name: 'NextGen', logo: <svg width="105" height="24" viewBox="0 0 105 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 0V24H8.57143L17.1429 8.57143V24H25.7143V0H17.1429L8.57143 15.4286V0H0ZM34.2857 24H42.8571V0H34.2857V24ZM60 24H51.4286V0H60V24ZM77.1429 24H85.7143V0H77.1429V24ZM105 12C105 18.6274 100.274 24 93.5714 24H88V0H93.5714C100.274 0 105 5.37258 105 12Z" fill="currentColor"/></svg> },
-  { name: 'Synergy', logo: <svg width="80" height="24" viewBox="0 0 80 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24V12H24C18.6274 12 12 6.62742 12 0ZM34.2857 24H42.8571V0H34.2857V24ZM58.7143 4.28571L51.4286 24H66L73.2857 4.28571L58.7143 4.28571ZM80 12C80 5.37258 74.6274 0 68 0H64V24H68C74.6274 24 80 18.6274 80 12Z" fill="currentColor"/></svg> }
+  { 
+    name: 'Partner 1', 
+    logo: <Image src="https://res.cloudinary.com/dvb1b2vgi/image/upload/v1762154044/IMG_2908_twmnr6.jpg" alt="Partner 1" fill className="object-contain rounded-lg" sizes="160px" /> 
+  },
+  { 
+    name: 'Partner 2', 
+    logo: <Image src="https://res.cloudinary.com/dvb1b2vgi/image/upload/v1762154043/IMG_2905_cjgq76.jpg" alt="Partner 2" fill className="object-contain rounded-lg" sizes="160px" /> 
+  },
+  { 
+    name: 'Partner 3', 
+    logo: <Image src="https://res.cloudinary.com/dvb1b2vgi/image/upload/v1762154043/IMG_2910_qbkeet.jpg" alt="Partner 3" fill className="object-contain rounded-lg" sizes="160px" /> 
+  },
+  { 
+    name: 'Partner 4', 
+    logo: <Image src="https://res.cloudinary.com/dvb1b2vgi/image/upload/v1762154043/IMG_2907_i62r1f.jpg" alt="Partner 4" fill className="object-contain rounded-lg" sizes="160px" /> 
+  },
+  { 
+    name: 'Partner 5', 
+    logo: <Image src="https://res.cloudinary.com/dvb1b2vgi/image/upload/v1762154042/IMG_2900_to3nx8.jpg" alt="Partner 5" fill className="object-contain rounded-lg" sizes="160px" /> 
+  },
+  { 
+    name: 'Partner 6', 
+    logo: <Image src="https://res.cloudinary.com/dvb1b2vgi/image/upload/v1762154042/IMG_2902_fwydth.jpg" alt="Partner 6" fill className="object-contain rounded-lg" /> 
+  }
 ];
 
 export default async function Home() {
@@ -91,7 +109,7 @@ export default async function Home() {
                 </p>
               </div>
               <div className="relative mt-12 w-full overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_10%,white_90%,transparent)]">
-                <ul className="flex w-max animate-marquee items-center [--duration:40s] hover:[animation-play-state:paused]">
+                <ul className="flex w-max animate-marquee items-center [--duration:30s] hover:[animation-play-state:paused]">
                   {[...companies, ...companies].map((company, index) => (
                     <CompanyLogo key={index}>{company.logo}</CompanyLogo>
                   ))}
