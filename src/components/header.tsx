91722c8f1b61d4fcb0b10af47b28a81cf833a01a
Rahul <kumarplayground12345@gmai.com>
2025-11-30 20:35:19 +0530

    Remove Jobs link from navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,6 @@
 import { HeaderClient } from "./header-client";
 
 const navLinks = [
-  { href: "/jobs", label: "Jobs" },
   { href: "/courses", label: "Courses" },
   { href: "/about", label: "About Us" },
   { href: "/contact", label: "Contact Us" },
